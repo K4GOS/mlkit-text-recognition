9fc040338658d6d98f6d8e0f3da50bc1c5409959
K4GOS <come.paya@gmail.com>
2024-12-27 21:10:12 +0100

    IOS now working after clone thanks to the MlkitTextRecognition.h file

---
+++ ios/MlkitTextRecognition.h
@@ -0,0 +1,12 @@
+//
+//  MlkitTextRecognition.h
+//  MlkitTextRecognition
+//
+//  Created by Côme Paya on 27/12/2024.
+//
+
+#ifndef MlkitTextRecognition_h
+#define MlkitTextRecognition_h
+
+
+#endif /* MlkitTextRecognition_h */
