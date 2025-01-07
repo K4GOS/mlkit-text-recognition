aeece3447e508eda5f101992024aa94b7ddec812
K4GOS <come.paya@gmail.com>
2025-01-07 20:18:56 +0100

    Fixed objective C import to make it work both for the example app of the expo module and when we npm install the module in another app

--- ios/VisionCameraTextRecognition.m
+++ ios/VisionCameraTextRecognition.m
@@ -9,11 +9,17 @@
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/FrameProcessorPluginRegistry.h>
 
-#import "MlkitTextRecognition-Swift.h"
 #import "VisionCameraTextRecognition-Bridging-Header.h"
 
-// // Example for a Swift Frame Processor plugin automatic registration
-//VISION_EXPORT_SWIFT_FRAME_PROCESSOR(VisionCameraTextRecognition, getTextBlocksFromFrame)
+// if else to make it work both for the example app of the expo native module
+// and when we npm install it in another app
+#if __has_include("MlkitTextRecognition/MlkitTextRecognition-Swift.h")
+#import "MlkitTextRecognition/MlkitTextRecognition-Swift.h"
+#else
+#import "MlkitTextRecognition-Swift.h"
+#endif
+
+// We export our swift plugin
 @interface VisionCameraTextRecognition (FrameProcessorPluginLoader)
 @end
 
